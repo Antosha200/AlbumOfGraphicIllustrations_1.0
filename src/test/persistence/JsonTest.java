d56b972c0ff8daf202ee090c8ee7c87dde9f0a72
antosha2000 <anton2-2000@mail.ru>
2022-05-10 19:19:00 +0300

    Finale version

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -4,11 +4,6 @@ import model.Photo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * JsonTest
- * Modelled based on JsonSerializationDemo https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
- */
-
 public class JsonTest {
     protected void checkPhoto(String name, Photo photo) {
         assertEquals(name, photo.getName());
